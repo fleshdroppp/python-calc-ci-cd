bd835f73fb29abf31df25dd23f285a5499fd5706
fleshdroppp <khusnutdinovakim@gmail.com>
2022-11-27 16:26:15 +0300

    main.py and test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import Calculator
+
+
+class CalculatorTest(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.add(4, 4), 8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(8, 7), 56)
+
+    def test_substract(self):
+        self.assertEqual(self.calculator.substract(-3, 7), -10)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(2, 10), 0.2)
+
+
+unittest.main()
